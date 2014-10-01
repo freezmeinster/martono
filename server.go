97deae810d353dcf4128c386a6c0b9b4bfeafc5c
freez_meinster <freez_meinster@yahoo.co.id>
2014-10-02 06:33:54 +0700

    instirahat dulu

--- server.go
+++ server.go
@@ -3,20 +3,29 @@ package main
 import (
     "fmt"
     "github.com/go-martini/martini"
+    "github.com/martini-contrib/render"
 )
 
 func HelloAction(params martini.Params) string {
     return fmt.Sprintf("Hallo %s", params["name"])
 }
 
+func RenderAction(r render.Render){
+   r.HTML(200, "index", "Bram")
+}
+
+
 func main(){
     m := martini.Classic()
+    m.Use(render.Renderer())
+    
     m.Get("/",func() string{
         return "Nguknguk"
     })
     
     m.Group("/tools", func(r martini.Router){
         r.Get("/hello/:name", HelloAction)
+        r.Get("/render", RenderAction)
     })
     m.Run()
 }
